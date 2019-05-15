4918acc7dda5113775dcd433eb844557579b58f9
Todinho <gabrieltosi5@gmail.com>
2019-05-14 21:59:22 -0300

    Update Escalonador.java

--- src/escalonador/Escalonador.java
+++ src/escalonador/Escalonador.java
@@ -2,6 +2,13 @@ package escalonador;
 
 public class Escalonador {
 	public static void main(String[] args) {
-		
+		//criar fila e gerenciador
+		//verificar arquivo
+		//guardar valores --> vetor de chegada, vetor de duraçao e matriz de I/O
+		//verificar o vetor chegada
+		//arrumar fila de chegada
+		//começar a simulacao com o quantum fornecido
+		//verificar I/O no vetor de I/O do processo
+		//imprimir qual o processo esta no CPU, qual esta na fila e qual a duraçao
 	}
 }
